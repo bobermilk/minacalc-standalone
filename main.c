eee71cf8dd6d1adc98b35f863579d373aa2cbba6
bobermilk <ppcat39@gmail.com>
2023-02-22 09:05:56 +0800

    print calc version

--- main.c
+++ main.c
@@ -3,5 +3,9 @@
 #include "API.h"
 
 int main(int argc, char* argv[]) {
-    calc_file(argv[1], strtof(argv[2], NULL), strtof(argv[3], NULL));
+    if(argc==1){
+        printf("%d", calc_version());
+    } else {
+        calc_file(argv[1], strtof(argv[2], NULL), strtof(argv[3], NULL));
+    }
 }
